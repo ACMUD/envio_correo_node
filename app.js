4ff65c9bca8580121f2062ceea4e47d27f1bb73b
migg-src <migg.ro3@gmail.com>
2019-07-16 21:41:10 -0500

    Server configuration finished.

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const messageConfig = require('./messageConfig');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors())
+
+app.post('/formulario', (req, res) => {
+    messageConfig(req.body);
+    res.status(200).send();
+})
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo')
+});
